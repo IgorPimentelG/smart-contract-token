1facc868e77dfed7be87d6cbf706a3e9509844a4
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-05-27 10:40:50 -0300

    feat: add unit test

--- test/GLDToken.test.js
+++ test/GLDToken.test.js
@@ -11,10 +11,30 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("All tokens should be in my account", async () => {
         const instance = await GLDToken.deployed();
         const totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("Is possible to send tokens between accounts", async () => {
+        const sendTokens = 1;
+        const instance = await GLDToken.deployed();
+        const totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+    });
+
+    it("Is not possible to send more tokens than available in total", async () => {
+        const instance = await GLDToken.deployed();
+        const balanceOfDeployer = await instance.balanceOf(deployerAccount);
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
     });
 });
 
