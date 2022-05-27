40c9a11378c3a56a8b90bb49de43594e2b7a8bc8
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2022-05-27 18:12:35 -0300

    feat: add frontend

--- contracts/GLDTokenSale.sol
+++ contracts/GLDTokenSale.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.14;
 
 import "./Crowdsale.sol";
+import "./KycContract.sol";
 
 contract GLDTokenSale is Crowdsale {
+
+    KcyContract kyc;
+
     constructor(
         uint256 rate,
         address payable wallet,
-        IERC20 token
-    ) Crowdsale(rate, wallet, token)  { }
+        IERC20 token,
+        KcyContract _kyc
+    ) Crowdsale(rate, wallet, token)  {
+        kyc = _kyc;
+     }
+
+     function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override {
+         super._preValidatePurchase(beneficiary, weiAmount);
+         require(kyc.kycCompleted(msg.sender), "KYC not completed, purchase not allowed");
+     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,14 @@
 const GLDToken = artifacts.require("GLDToken.sol");
 const GLDTokenSale = artifacts.require("GLDTokenSale.sol");
+const KycContract = artifacts.require("KycContract");
+
 require("dotenv").config({path: "../.env"});
 
 module.exports = async (deployer) => {
     const addr = await web3.eth.getAccounts();
     await deployer.deploy(GLDToken, process.env.INITIAL_TOKENS);
-    await deployer.deploy(GLDTokenSale, 1, addr[0], GLDToken.address);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(GLDTokenSale, 1, addr[0], GLDToken.address, KycContract.address);
     const instance = await GLDToken.deployed();
     await instance.transfer(GLDTokenSale.address, process.env.INITIAL_TOKENS);
 }
